b67590759bc62a08a4231a60690ffba7eec82b76
RobDawg-sama <pickles40324@gmail.com>
2022-11-12 06:54:51 -0500

    footer styles

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -2,25 +2,19 @@ import React, { Component } from "react";
 import Footer from "../footer/footer";
 import Navbar from "../navigation/navbar";
 import ContactForm from "../form/contact-form";
-import {useEffect} from "react";
-console.log(
-  document.getElementById("left-nav-wrapper")
-);
-export default function() {
-    useEffect(() => {
-        
-            document.getElementById("left-nav-wrapper").style.display="none";
-        
-      });
-    return (
-      <div className="contact-page-wrapper">
-        <div className="contact-center">
-          <ContactForm></ContactForm>
-        </div>
-        <div className="footer">
-          <Footer />
-        </div>
+import { useEffect } from "react";
+export default function () {
+  useEffect(() => {
+    document.getElementById("left-nav-wrapper").style.display = "none";
+  });
+  return (
+    <div className="contact-page-wrapper">
+      <div className="contact-center">
+        <ContactForm></ContactForm>
       </div>
-    );
-  }
-
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
